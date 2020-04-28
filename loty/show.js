bddae6a3dd3c1558d150297183f842ea1bcfb837
Kejmer <kajetanhus@radgost.com>
2020-04-28 20:12:20 +0200

    krok 4

--- loty/show.js
+++ loty/show.js
@@ -4,10 +4,13 @@ var biggest = "";
 function isPassenger(object) {
     return object.getAttribute("data-indetyfikator-pasazera") !== "undefined";
 }
+var passIdArr = [];
 passengers.forEach(function (child) {
     if (isPassenger(child))
-        console.log(child.dataset.identyfikatorPasazera);
+        passIdArr.push(child.dataset.identyfikatorPasazera);
 });
+passIdArr.sort().reverse();
+console.log(passIdArr[0]);
 // for(let child of passengers) {
 //   // if (biggest < child.dataset.indentyfikatorPasazera) {
 //     biggest = child;
--- loty/show.ts
+++ loty/show.ts
@@ -14,12 +14,17 @@ function isPassenger(object: any): object is Passenger {
   return object.getAttribute("data-indetyfikator-pasazera") !== "undefined"
 }
 
+let passIdArr: Array<string> = []
 passengers.forEach(
   function(child) {
     if (isPassenger(child))
-      console.log(child.dataset.identyfikatorPasazera);
+      passIdArr.push(child.dataset.identyfikatorPasazera);
   }
 );
+
+passIdArr.sort().reverse();
+
+console.log(passIdArr[0]);
 
 // for(let child of passengers) {
 //   // if (biggest < child.dataset.indentyfikatorPasazera) {
